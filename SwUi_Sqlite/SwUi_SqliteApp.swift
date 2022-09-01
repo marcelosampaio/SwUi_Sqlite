dc35d497f006e298b05d9d7a4d47f3afa694951e
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-09-01 12:11:23 -0300

    Initial Commit

---
+++ SwUi_Sqlite/SwUi_SqliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUi_SqliteApp.swift
+//  SwUi_Sqlite
+//
+//  Created by Marcelo Sampaio on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SwUi_SqliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
